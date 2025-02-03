c9f094008b469c54f265f58dedf4c94839104270
nataly-linux <rojasmiranda990@gmail.com>
2025-02-03 17:27:17 -0500

    import and export

---
+++ js/frutas.js
@@ -0,0 +1,14 @@
+//  const sandia="🍉";
+//  export default sandia;
+export function pintarPlatano(){
+    console.log("🍌");
+}
+
+export const frutilla=()=>{
+    console.log("🍓")
+}
+export class Fruta{
+    constructor(nombre){
+        this.nombre=nombre;
+    }
+}
